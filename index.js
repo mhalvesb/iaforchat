fd91d1f2bce71b292f01aa9b58642b1c6f8eff72
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-05-24 17:38:28 -0300

    atualizado

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const corsOptions = {
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
-
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
