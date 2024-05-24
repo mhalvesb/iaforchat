1eedf4724c34bdf29f44d5d2ca08709ebaa960db
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-05-24 17:42:22 -0300

    atualizado

--- index.js
+++ index.js
@@ -23,7 +23,14 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
 
 
 
-app.post("/", async (req, res) =>{
+app.post("/", async (req, res, next) =>{
+        res.header(
+            "Access-Control-Allow-Origin",
+            "React app URL"
+        );
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        next();
         const prompt = req.body.message;
         const result = await model.generateContent(prompt);
         res.json({data: result.response.text()});
