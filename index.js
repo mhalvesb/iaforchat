80c8ecdf85eba8eea1854e3044037bfba4b21b3a
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-05-24 18:28:45 -0300

    atualizado

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI("AIzaSyBkzd76HqIBXyiblabZBqEkGLA1FR6XpaE");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://chatbot-nine-self.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 app.get("/", (req, res) =>{
