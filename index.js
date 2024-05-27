86c8793ea66be3197a1b9184776ee908e8b3cb5b
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-05-27 17:01:52 -0300

    adicionada nova função

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI("AIzaSyBkzd76HqIBXyiblabZBqEkGLA1FR6XpaE");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
 
+
+
+
+
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://chatbot-nine-self.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
@@ -31,23 +36,18 @@ app.use(function(req, res, next) {
 
 
 app.get("/", async (req, res) =>{
-    res.json({string: "ok"});
     const result = await model.generateContent("Você é uma I.A que trabalha para um petshop e temos estoque de produtos de cachorro e gatos. O nome da loja é Cachorrinhos e o telefone é (21)98286-0764");
     res.json({data: result.response.text()});
 });
 
 app.post("/", async (req, res, next) =>{
-        
-    
+    const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-flash",
+        systemInstruction: "Você é uma I.A que trabalha para um petshop e temos estoque de produtos de cachorro e gatos. O nome da loja é Cachorrinhos e o telefone é (21)98286-0764.\nTemos ração, Golden Special por 133 reais, Premier Pet por 254 reais, Royal Canin por 555 reais, Whiskas por 157 reais.",
+      });
         const prompt = req.body.message;
         const result = await model.generateContent(prompt);
         res.json({data: result.response.text()});
-        if(prompt === undefined || prompt === "" || prompt === null)
-    {
-        const result = await model.generateContent("Você é uma I.A que trabalha para um petshop e temos estoque de produtos de cachorro e gatos. O nome da loja é Cachorrinhos e o telefone é (21)98286-0764");
-        res.json({data: result.response.text()});
-    }
-        
 });
 
 app.listen(8080, () =>{
